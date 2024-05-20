1cf0cfad3f217a00bc6b56b95a27cb4e18f1839c
wael <78425538+wa20@users.noreply.github.com>
2024-05-20 18:09:30 +0100

    updated translate page to fetch languages

--- translato/app/translate/page.tsx
+++ translato/app/translate/page.tsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import TranslationForm from "@/components/TranslationForm";
 
-function TranslatePage() {
+export type TranslationLanguages = {
+  translation: {
+    [key: string]: {
+      name: string;
+      nativeName: string;
+      dir: "ltr" | "rtl";
+    };
+  };
+};
+
+async function TranslatePage() {
+  auth().protect();
+
+  const { userId } = auth();
+  if (!userId) throw new Error("User not logged in");
+
+  const languagesEndpoint =
+    "https://api.cognitive.microsofttranslator.com/languages?api-version=3.0";
+
+  const response = await fetch(languagesEndpoint, {
+    next: {
+      revalidate: 60 * 60 * 24, // refresh once every 24 hours, cach data
+    },
+  });
+
+  const languages = (await response.json()) as TranslationLanguages;
+
   return (
-    <div>TranslatePage</div>
-  )
+    <div className="">
+      <TranslationForm languages={languages}/>
+    </div>
+  );
 }
 
-export default TranslatePage
+export default TranslatePage;
