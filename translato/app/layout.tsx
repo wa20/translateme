59f2b02adf99aa2a2c9907dd6f4a4e5517694c11
wael <78425538+wa20@users.noreply.github.com>
2024-05-20 18:06:43 +0100

    created header component

--- translato/app/layout.tsx
+++ translato/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from '@clerk/nextjs'
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,13 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+         <Header/>
+        <div className="max-w-6xl mx-auto">
+          {children}
+        </div>
+        
+        </body>
     </html>
     </ClerkProvider>
   );
---
+++ translato/components/Header.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import Link from "next/link";
+import Image from "next/image";
+import { SignInButton, UserButton } from "@clerk/nextjs";
+
+function Header() {
+  const { userId } = auth();
+
+  return (
+    <header className="flex items-center justify-between px-8 border-b mb-5 shadow-md static">
+        <div className="flex items-center justify-center overflow-hidden h-20">
+            <Link href='/'>
+            <Image 
+             src='https://links.papareact.com/xgu'
+             alt="logo"
+             width={200}
+             height={100}
+             className="object-contain h-32 cursor-pointer"
+            />
+        </Link>
+        </div>
+        
+        {userId ? (
+            <div>
+                <UserButton />
+            </div>
+        ) : (
+            <SignInButton 
+            signUpForceRedirectUrl="/translate"
+            mode="modal" >
+                <button className=" flex items-center px-3 py-2 rounded-md shadow-md bg-blue-700 text-white font-semibold hover:shadow-xl active:scale-90 duration-150 ease-out">Log In</button>
+            </SignInButton>
+        )
+        
+    }
+    </header>
+  );
+}
+
+export default Header;
