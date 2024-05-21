75d4c3cb1a9eff10f8ec0de5ca50ea358886098d
wael <78425538+wa20@users.noreply.github.com>
2024-05-21 23:07:40 +0100

    created Recorder component

--- translato/components/Recorder.tsx
+++ translato/components/Recorder.tsx
@@ -1,20 +1,23 @@
+"use client";
 
-'use client'
+import { MicIcon } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { useFormStatus } from "react-dom";
 
-import { MicIcon } from 'lucide-react'
-import React, { useState, useEffect } from 'react'
-import { useFormStatus } from 'react-dom';
+export const mimeType = "audio/webm";
 
-function Recorder( {uploadAudio}: { uploadAudio: (blob: Blob) => void } ){
-    const [permission, setPermission] = useState(false);
-    const [stream, setStream] = useState<MediaStream | null>(null);
-    const [recordingStatus, setRecordingStatus] = useState("inactive")
-    const { pending } = useFormStatus()
+function Recorder({ uploadAudio }: { uploadAudio: (blob: Blob) => void }) {
+  const [permission, setPermission] = useState(false);
+  const [stream, setStream] = useState<MediaStream | null>(null);
+  const [recordingStatus, setRecordingStatus] = useState("inactive");
+  const [audioChunks, setAudioChunks] = useState<Blob[]>([]);
 
-
+  const { pending } = useFormStatus();
+  const mediaRecorder = useRef<MediaRecorder | null>(null);
+  
   useEffect(() => {
-        getMicrophonePermission()
-  }, [])
+    getMicrophonePermission();
+  }, []);
 
   const getMicrophonePermission = async () => {
     if ("MediaRecorder" in window) {
@@ -32,28 +35,82 @@ function Recorder( {uploadAudio}: { uploadAudio: (blob: Blob) => void } ){
       alert("Your browser does not support the MediaRecorder API");
     }
   };
-    
+
+  const startRecording = async () => {
+    if (stream === null || pending) return;
+
+    setRecordingStatus("recording");
+
+    // Create a new media recorder instance using the stream
+    const media = new MediaRecorder(stream, { mimeType });
+    mediaRecorder.current = media;
+    mediaRecorder.current.start();
+
+    let localAudioChunks: Blob[] = [];
+
+    mediaRecorder.current.ondataavailable = (event) => {
+      if (typeof event.data === "undefined") return;
+      if (event.data.size === 0) return;
+
+      localAudioChunks.push(event.data);
+    };
+
+    setAudioChunks(localAudioChunks);
+  };
+
+  const stopRecording = async () => {
+    if (mediaRecorder.current === null || pending) return;
+
+    setRecordingStatus("inactive");
+    mediaRecorder.current.stop();
+    mediaRecorder.current.onstop = () => {
+      const audioBlob = new Blob(audioChunks, { type: mimeType });
+      uploadAudio(audioBlob);
+      setAudioChunks([]);
+    };
+  };
 
   return (
-    <div>
-        <MicIcon size={20} className='group-hover:underline'/>
+    <div
+      className={`flex items-center group text-blue-500 cursor-pointer border rounded-md w-fit px-3 py-2  mb-5 ${
+        recordingStatus === "recording"
+          ? "bg-red-500 text-white"
+          : "hover:bg-[#E7F0FE]"
+      }`}
+    >
+      <MicIcon size={20} className="  group-hover:underline" />
 
-        {!permission && (
-            <button onClick={getMicrophonePermission}>Get Microphone</button>
-        )}
+      {!permission && (
+        <button onClick={getMicrophonePermission}>Get Microphone</button>
+      )}
 
-        {{pending && (
-            <p>
-                {recordingStatus === "recording"
-                    ? "Recording..."
-                    : "Stopping recording..."
-                }
-            </p>
-        )}}
+      {pending && (
+        <p>
+          {recordingStatus === "recording"
+            ? "Recording..."
+            : "Stopping recording..."}
+        </p>
+      )}
 
-        
+      {permission && recordingStatus === "inactive" && !pending && (
+        <button
+          onClick={startRecording}
+          className="text-sm font-medium group-hover:underline ml-2 mt-1"
+        >
+          Speak
+        </button>
+      )}
+
+      {recordingStatus === "recording" && (
+        <button
+          onClick={stopRecording}
+          className="text-sm font-medium group-hover:underline ml-2 mt-1"
+        >
+          Stop
+        </button>
+      )}
     </div>
-  )
+  );
 }
 
-export default Recorder
+export default Recorder;
