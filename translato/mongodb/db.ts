cfd8d6e707957a2b24168547a00b44bf03e4e0e8
wael <78425538+wa20@users.noreply.github.com>
2024-05-21 11:47:16 +0100

    created mongodb connection

---
+++ translato/mongodb/db.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+
+// const connectionString = `mongodb+srv://${process.env.MONGO_DB_USERNAME}:${process.env.MONGO_DB_PASSWORD}@cluster0.erhupcn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+const connectionString = process.env.MONGO_DB_URI
+
+if(!connectionString) {
+    throw new Error ("Define correct environment variables")
+}
+
+const connectDB = async () => {
+     if(mongoose.connection?.readyState >= 1){
+        console.log("---- Conencted to MongoDB----")
+     }
+
+    try {
+        await mongoose.connect(connectionString);
+        console.log("--- Connection started with MongoDB ---")
+    } catch (error) {
+        console.error("Could not conenct to MongoDB", error)
+        
+    }
+
+};
+
+export default connectDB
