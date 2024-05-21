068124a817fe0b31360d7b20007218ac50d3c5d3
wael <78425538+wa20@users.noreply.github.com>
2024-05-21 23:29:30 +0100

    updated home page

--- translato/app/page.tsx
+++ translato/app/page.tsx
@@ -1,15 +1,37 @@
+import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
-import Link from "next/link"
-import TranslatePage from "./translate/page";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = auth();
+
   return (
-    <main className="">
-      <h1>Translato</h1>
-
-      <Link href="/translate">
-        Go to translate page
-      </Link>
+    <main className="flex flex-col items-center justify-center p-10">
+      <h1 className="text-3xl lg:text-6xl text-center pb-10 mb-5 font-light">
+        Understand your world and communicate across languages
+      </h1>{" "}
+      <Image
+        src="https://links.papareact.com/ert"
+        alt="logo"
+        width={700}
+        height={700}
+      />
+      {userId ? (
+        <Link
+          href="/translate"
+          className="bg-blue-500 hover:bg-blue-600 w-full mt-10 lg:w-fit p-5 rounded-md text-white text-center cursor-pointer hover:shadow-xl active:scale-90 duration-150 ease-out"
+        >
+          Translate Now
+        </Link>
+      ) : (
+        <Button className="bg-blue-500 hover:bg-blue-600 w-full mt-10 lg:w-fit p-5 hover:shadow-xl active:scale-90 duration-150 ease-out">
+          <SignInButton forceRedirectUrl="/translate" mode="modal">
+            Sign In to Get Translating
+          </SignInButton>
+        </Button>
+      )}
     </main>
   );
 }
