60a435ee0b0ec6eddce64d80ca9388a346be820d
wael <78425538+wa20@users.noreply.github.com>
2024-05-21 16:24:11 +0100

    created inital styling for translation history containers

--- translato/components/TranslationHistory.tsx
+++ translato/components/TranslationHistory.tsx
@@ -3,6 +3,12 @@
 import { ITranslation } from '@/mongodb/models/User'
 import { auth } from '@clerk/nextjs/server'
 import React from 'react'
+import TimeAgoText from './TimeAgoText';
+
+const getLanguage = (code: string) => {
+    const lang = new Intl.DisplayNames(['en'], {type: "language"});
+    return lang.of(code);
+}
 
 async function TranslationHistory() {
     const { userId } = auth()
@@ -19,7 +25,43 @@ async function TranslationHistory() {
 
     console.log('translation hitory: ', translations)
     return (
-        <div>TranslationHistory</div>
+        <div>
+            <h1 className='text-3xl my-5'>Translation History</h1>
+
+            {translations.length === 0 && (
+                <p className='mb-5 text-gray-400'>You currently have no translations.</p>
+            )}
+
+            <ul className='divide-y border rounded-md'>
+                {translations.map((translation) => (
+                    <li 
+                    key={translation._id}
+                    className='flex justify-between items-centerp-5 hover:bg-gray-50 relative'
+                    
+                    >
+                        <p className='text-sm mb-5 text-gray-500'>
+                            {getLanguage(translation.from)}
+                            {" -> "}
+                            {getLanguage(translation.to)}
+                        </p>
+                        <div className='space-y-2 pr-5'>
+                            <p>{translation.fromText}</p>
+                            <p className='text-gray-400'>{translation.toText}</p>
+                        </div>
+
+                        {/* TODO add TimeAgo component  */}
+                        <p className='absolute top-2 right-2 text-gray-300 text-sm'>
+                            <TimeAgoText date={new Date(translation.timestamp).toISOString()}></TimeAgoText>
+                        </p>
+                        
+
+                        {/* TODO add delete button */}
+
+
+                    </li>
+                ))}
+            </ul>
+        </div>
     ) 
 }
 
